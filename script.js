f28c57e71c635b965ba0ff72050e6755d150c3c7
Cristian Fabiano Eisenhut <32318124+cristiancfe@users.noreply.github.com>
2025-11-05 12:00:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ document.getElementById('form-contato').addEventListener('submit', function (e) 
 	if (nome && email && mensagem) {
 	  alert('Mensagem enviada com sucesso! Entrarei em contato em breve.');
 	  document.getElementById('form-contato').reset();
-		 window.location.href = 'https://minhaempresanarede.vercel.app/';
+	  window.location.href = 'index.html';
 	} else {
 	  alert('Por favor, preencha todos os campos obrigatórios.');
 	}
 
   });
+
 
 
